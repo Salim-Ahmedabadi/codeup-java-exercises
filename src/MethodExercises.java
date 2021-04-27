09ffdf3a9feb42f12da9e6625ba3b8faba3aceda
Salim Ahmedabadi <ahmedabadi.salim@gmail.com>
2021-04-27 12:49:39 -0500

    feat: Started working on MethodExercise

---
+++ src/MethodExercises.java
@@ -0,0 +1,21 @@
+public class MethodExercises {
+    public static void main(String[] args){
+        System.out.println(Addition(2,3));
+        System.out.println(Substraction(2,3));
+        System.out.println(Multiplication(2,3));
+        System.out.println(Division(2,3));
+    }
+    public static int Addition(int num1, int num2) {
+        return num1 + num2;
+
+    }public static int Substraction(int num1, int num2) {
+        return num1 - num2;
+
+    }public static int Multiplication(int num1, int num2){
+        return num1 * num2;
+    }
+    public static int Division(int num1, int num2) {
+        return num1 / num2;
+    }}
+
+
