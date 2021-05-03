1a471c1cb05d245ff8594428d3898585311d8a75
Salim Ahmedabadi <ahmedabadi.salim@gmail.com>
2021-05-03 10:32:36 -0500

    feat: Working on OOPs Exercise

---
+++ src/Person.java
@@ -0,0 +1,22 @@
+public class Person {
+    public static void main(String[] args){
+        Person salim = new Person();
+        salim.name = "Salim";
+        System.out.println(salim.getName());
+    }
+
+    private String name;
+        public String getName(){
+//TODO: return the person's name
+
+        }
+
+        public void setName(String name){
+//TODO: change the name property to the passed value
+
+        }
+        public void sayHello(){
+//TODO: print a message to the console using the person's name
+        }
+    }
+
