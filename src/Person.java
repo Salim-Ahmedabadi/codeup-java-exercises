a3f6fead9c959b63adf1a8c3b5c378a9002db3f4
Salim Ahmedabadi <ahmedabadi.salim@gmail.com>
2021-05-03 11:27:08 -0500

    feat: Created util package with Input class

--- src/Person.java
+++ src/Person.java
@@ -3,20 +3,23 @@ public class Person {
         Person salim = new Person();
         salim.name = "Salim";
         System.out.println(salim.getName());
+        System.out.println(salim.setName("Sunny"));
     }
 
     private String name;
         public String getName(){
 //TODO: return the person's name
-
+        return name;
         }
 
-        public void setName(String name){
+        public String setName(String name){
 //TODO: change the name property to the passed value
-
+            this.name = name;
+                return name;
         }
         public void sayHello(){
 //TODO: print a message to the console using the person's name
+
         }
     }
 
