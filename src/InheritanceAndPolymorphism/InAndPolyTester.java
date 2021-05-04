3b55a9b01fbfb91fa09612df3b377d49933ec01c
Salim Ahmedabadi <ahmedabadi.salim@gmail.com>
2021-05-04 15:34:08 -0500

    feat: Modified InheritanceAndPolymorphism Lecture

--- src/InheritanceAndPolymorphism/InAndPolyTester.java
+++ src/InheritanceAndPolymorphism/InAndPolyTester.java
@@ -1,6 +1,9 @@
 package InheritanceAndPolymorphism;
 import oopLecture.Person;
+import java.util.Locale;
+
 public class InAndPolyTester {
+
     public static void main(String[] args) {
         Person person = new Person("Douglas", "Hirsh");
         //person.firstName = "Douglas";
@@ -17,7 +20,47 @@ public class InAndPolyTester {
         employee.doWork();
 
         System.out.println("employee.sayHello() = " + employee.sayHello());
+
+        Vehicle genericVehicle = new Vehicle();
+        genericVehicle.start();
+
+        Bus bus = new Bus(80);
+        bus.setHeadlightsOn(true);
+        bus.start();
+        bus.honkHorn();
+        bus.setNumberOfOccupants(2);
+        System.out.println("Zooming in a bus at " + bus.getMaxSpeed() + " MPH with " + bus.getNumberOfOccupants() + " occupants inside.");
+
+        Jet fastJet = new Jet(2000);
+        fastJet.setNumberOfOccupants(1);
+        fastJet.start();
+        System.out.println("Zooming in a fast jet at " + fastJet.getMaxSpeed() + " MPH with " + fastJet.getNumberOfOccupants() + " occupants inside.");
+
+        System.out.println();
+        Bus fasterBus = (Bus)spawnVehicle("bus", 120);
+        fasterBus.honkHorn();
+        fasterBus.start();
+
+        Vehicle slowerJet = spawnVehicle("JeT", 700);
+        slowerJet.setNumberOfOccupants(2);
+        slowerJet.start();
     }
 
-}
+    public static Vehicle spawnVehicle(String vehicleType, int maxSpeed) {
+        Vehicle vehicle = null;
 
+        switch (vehicleType.toLowerCase()) {
+            case "vehicle":
+                vehicle = new Vehicle(maxSpeed);
+                break;
+            case "bus":
+                vehicle = new Bus(maxSpeed);
+                break;
+            case "jet":
+                vehicle = new Jet(maxSpeed);
+                break;
+        }
+
+        return vehicle;
+    }
+}
