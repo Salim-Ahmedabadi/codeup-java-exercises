b1ac8fab3492f72791affad84e6d40645a58762c
Salim Ahmedabadi <ahmedabadi.salim@gmail.com>
2021-05-06 11:52:31 -0500

    feat: Added Exercises pending

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,7 +1,37 @@
 package util;
-
+import java.util.Scanner;
 public class Input {
+    //instance variable (private)
+    private static Scanner sc = new Scanner(System.in);
+
+    public static String getString(){
+
+        System.out.println("Give me a string");
+        String string = sc.nextLine();
+        System.out.println(string);
+        return string;
+    }
+
+    public static boolean yesNo(){
+        System.out.println("\nContinue?");
+        String input = sc.next();
+        if(input.startsWith("y")){
+         System.out.println("true");
+            return true;
+        } else {
+          System.out.println("false");
+            return false;
+        }
+    }
+    public static int getInt(){
+        int input = sc.nextInt();
+        return input;
+    }
+
+    public static double getDouble(){
+        double inputDouble = sc.nextDouble();
+        return inputDouble;
+    }
 
 
 }
-
